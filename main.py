43c179c03e64a4c696e69259370ab8d7bd57d8e3
Peter Messay <165043083+petermesy@users.noreply.github.com>
2025-06-23 09:25:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
